fd49a99b50672045b085d24e49b4e8800e5f2332
Antoinette-Cordis <133567517+Antoinette-Cordis@users.noreply.github.com>
2023-06-04 02:49:35 -0700

    Weather forecast function (JS),HTML and CSS were reviewed.

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function formatDate(timestamp) {
   return `${year},${day} ${hours}:${minutes}`;
 }
 function formatDay(timestamp) {
-  let date = newDate(timestamp * 1000);
+  let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Thurs", "Fri", "Sat", "Sun", "Mon"];
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
   return days[day];
 }
 function displayForecast(response) {
@@ -24,8 +24,9 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
-      forecastHTML.forecastHTML +
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
         `<div class="col-2">
               <div class="weather-forecast-dates">${formatDay(
                 forecastDay.dt
@@ -53,12 +54,13 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function showForecast(coordinates) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=bd5b4461863eddaa6ced0a0a67989e0a&units=metric`;
+  console.log(coordinates);
+  let Apikey = "bd5b4461863eddaa6ced0a0a67989e0a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${Apikey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 function showTemperature(response) {
-  console.log(response.data);
   let temperature = document.querySelector("#temperature");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
@@ -67,21 +69,20 @@ function showTemperature(response) {
   let description = document.querySelector("#description");
   temperature.innerHTML = Math.round(response.data.main.temp);
   let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
   humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
   city.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   date.innerHTML = formatDate(response.data.dt * 1000);
-
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
   showForecast(response.data.coord);
 }
 function search(city) {
   let Apikey = "bd5b4461863eddaa6ced0a0a67989e0a";
-  let Apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Apikey}&units=metrics`;
+  let Apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Apikey}&units=metric`;
   axios.get(Apiurl).then(showTemperature);
 }
 function searchForm(event) {
